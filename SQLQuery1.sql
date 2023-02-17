28aca33c4d97e80b3106216642f8e1fa6b95b26f
Neil Maddox <34941165+TheNeilMaddox@users.noreply.github.com>
2023-02-17 08:30:43 -0500

    Updated SQL Query
    Added Comments

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,4 @@
+--Aggregating Averages
 WITH avg_permit_time AS (
   SELECT
     [ahj],[state],
@@ -16,9 +17,11 @@ WITH avg_permit_time AS (
     )
   ) AS avg_permit_time
   FROM [SolarData2]
-  GROUP BY [ahj],[state]), --Group by AHJ and State are required for unique key later in the joins. There can be several AHJ's with same names. (i.e. Clark County NV & WI)
+  GROUP BY [ahj],[state]), 
+  --Group by AHJ and State are required for unique key later in the joins.
+  --There can be several AHJ's with same names. (i.e. Clark County NV & WI)
 
- avg_preinstall_time AS (
+avg_preinstall_time AS (
 	Select
 		[ahj],[state],
 		avg(coalesce(
@@ -38,45 +41,45 @@ WITH avg_permit_time AS (
 	from [SolarData2]
 	GROUP BY [ahj],[state]),
 
-	avg_inspection_time AS (
-		Select
-			[ahj],[state],
-				avg(coalesce(
-					[Median Inspection Time 2019 0-10kW],
-					[Median Inspection Time 2019 11-50kW],
-					[Median Inspection Time 2020 0-10kW],
-					[Median Inspection Time 2020 11-50kW],
-					[Median Inspection Time 2021 0-10kW],
-					[Median Inspection Time 2021 11-50kW],
-					[Median Inspection Time 2018 11-50kW],
-					[Median Inspection Time 2018 0-10kW],
-					[Median Inspection Time 2017 11-50kW],
-					[Median Inspection Time 2017 0-10kW],
-					0
-				)
-			)AS avg_inspection_time
-			from [SolarData2]
-			group by [ahj],[state]),
+avg_inspection_time AS (
+	Select
+		[ahj],[state],
+			avg(coalesce(
+				[Median Inspection Time 2019 0-10kW],
+				[Median Inspection Time 2019 11-50kW],
+				[Median Inspection Time 2020 0-10kW],
+				[Median Inspection Time 2020 11-50kW],
+				[Median Inspection Time 2021 0-10kW],
+				[Median Inspection Time 2021 11-50kW],
+				[Median Inspection Time 2018 11-50kW],
+				[Median Inspection Time 2018 0-10kW],
+				[Median Inspection Time 2017 11-50kW],
+				[Median Inspection Time 2017 0-10kW],
+				0
+			)
+		)AS avg_inspection_time
+		from [SolarData2]
+		group by [ahj],[state]),
 
-	avg_pto_time AS (
-		Select
-			[ahj],[state],
-				avg(coalesce(
-					[Median Final IX to PTO 2021 11-50kW],
-					[Median Final IX to PTO 2021 0-10kW],
-					[Median Final IX to PTO 2020 11-50kW],
-					[Median Final IX to PTO 2020 0-10kW],
-					[Median Final IX to PTO 2019 11-50kW],
-					[Median Final IX to PTO 2019 0-10kW],
-					[Median Final IX to PTO 2018 11-50kW],
-					[Median Final IX to PTO 2018 0-10kW],
-					[Median Final IX to PTO 2017 11-50kW],
-					[Median Final IX to PTO 2017 0-10kW],
-					0
-				)
-			)AS avg_pto_time
-			from [SolarData2]
-			group by [ahj],[state])
+avg_pto_time AS (
+	Select
+		[ahj],[state],
+			avg(coalesce(
+				[Median Final IX to PTO 2021 11-50kW],
+				[Median Final IX to PTO 2021 0-10kW],
+				[Median Final IX to PTO 2020 11-50kW],
+				[Median Final IX to PTO 2020 0-10kW],
+				[Median Final IX to PTO 2019 11-50kW],
+				[Median Final IX to PTO 2019 0-10kW],
+				[Median Final IX to PTO 2018 11-50kW],
+				[Median Final IX to PTO 2018 0-10kW],
+				[Median Final IX to PTO 2017 11-50kW],
+				[Median Final IX to PTO 2017 0-10kW],
+				0
+			)
+		)AS avg_pto_time
+		from [SolarData2]
+		group by [ahj],[state])
 
 Select distinct
 [SolarData2].[state],
@@ -84,7 +87,7 @@ SolarData2.ahj,
 [SolarData2].geo_id,
 [SolarData2].utility,
 [SolarData2].[Total Installs],
-
+--Rounding and Percentiles--
 round(avg_preinstall_time,2) as [AVG Preinstall Time],
 100-ntile(100) over (order by avg_preinstall_time) PreinstallPCT, 
 round(avg_permit_time,2) as [AVG Permit Time],
@@ -98,24 +101,24 @@ UTILITYData.[Response Time (Business days)] Interconnection,
 round(avg_inspection_time+avg_permit_time+avg_preinstall_time+
 avg_pto_time+UTILITYData.[Response Time (Business days)],1) Total,
 100-ntile(100) over (order by round(avg_inspection_time+avg_permit_time+avg_preinstall_time+avg_pto_time+UTILITYData.[Response Time (Business days)],1)) TotalPCT,
-
+--Qualitative Utility Data--
 UTILITYData.[Offset Policy in Place from Utiltiy] Offset_Policy,
 UTILITYData.[Offset Percentage],
 UTILITYData.[Level 1 Process Cutoff (KW)] KW_Limit,
 UTILITYData.[Approval to Build required from utility before install] Utility_Approval,
-
+--Case fixes Binary to Boolean--
 case
 	when AHJData.[Online Permitting]=1 THEN 'True'
 	when AHJData.[Online Permitting]=0 THEN 'False'
 	else 'N/A'
 End as Online_Permitting,
-
+--Rest of AHJ Data--
 AHJData.[Structural Review Required],
 AHJData.[Median Permit Cost],
 AHJData.[Number of Inspections Reqd]
 
 
-
+--Joins--
 from SolarData2
 	join avg_permit_time
 		on SolarData2.[ahj]+SolarData2.[state] = avg_permit_time.[ahj]+avg_permit_time.[state]
@@ -125,23 +128,10 @@ from SolarData2
 		on SolarData2.[ahj]+SolarData2.[state] = avg_pto_time.[ahj]+avg_pto_time.[state]
 	join avg_preinstall_time
 		on SolarData2.[ahj]+SolarData2.[state] = avg_preinstall_time.[ahj]+avg_preinstall_time.[state]
+--Full outer join due to abundance of supplimentary data that is still relevant to scope--
 	Full Outer join UTILITYData
 		on SolarData2.eia_id = UTILITYData.[EIA ID]
 	join AHJData
 		on SolarData2.ahj+SolarData2.[state] = AHJData.ahj+AHJData.[state]
 
 order by [SolarData2].[state], Total DESC
-
-
-
-
-
-
-
-
-
-
-
-
-
---NTILE(4)OVER (Partition by avg_preinstall_time order by SolarData2.utility) as Quartile,
